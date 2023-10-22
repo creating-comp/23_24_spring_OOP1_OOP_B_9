8da752787e82dafc16268d5db4b4c3d48d4ace49
creating-comp <101798435+creating-comp@users.noreply.github.com>
2023-10-22 18:10:43 +0300

    6th sample solved

---
+++ functions.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add int max_of_four(int a, int b, int c, int d) here.
+*/
+int max_of_four(int a,int b,int c,int d){
+  int max=a;
+  if(max<b||max<c||max<d){
+      max=b;
+      if(max<c||max<d){
+          max=c;
+          if(max<d){
+              return d;
+          }
+          else {
+             return max;
+          }
+      }
+      else {
+          return max;
+      }
+      
+  }
+  else{
+      return max;
+  }
+    
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
