368d0b67d060327dbd9c8954e57a9da7f37df036
creating-comp <101798435+creating-comp@users.noreply.github.com>
2023-10-22 18:02:06 +0300

    Create basic_data_types.cpp
    third sample solved

---
+++ basic_data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+int x=3;
+long int y=12345678912345;
+char ch='a';
+float z= 334.23;
+double k= 14049.30493;
+
+scanf("%d %ld %c %f %lf", &x, &y, &ch, &z, &k);
+printf("%d \n%ld \n%c \n%f \n%lf \n", x,y,ch,z,k) ;  
+    
+    
+return 0;
+}
